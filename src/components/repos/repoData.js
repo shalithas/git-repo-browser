a1a090da8187407d441682e798a99178c38fe2d8
Shalitha Senarathne <shalitha.senarathne@insead.edu>
2019-10-03 16:07:18 +0800

    add bearer token if available

--- src/components/repos/repoData.js
+++ src/components/repos/repoData.js
@@ -2,14 +2,25 @@ import Axios from "axios";
 
 const url = "https://api.github.com";
 
+
+const token = process.env.VUE_APP_AUTH_TOKEN;
+let config = {};
+if(token){
+  config = {
+    headers: {
+      'Authorization': `bearer ${token}`
+    }
+  };
+}
+
 export const getRepos = username => {
-  return Axios.get(`${url}/users/${username}/repos`).then(res => res.data);
+  return Axios.get(`${url}/users/${username}/repos`, {}, config).then(res => res.data);
 };
 
 export const getRepo = (username, repoName) => {
-  return Axios.get(`${url}/repos/${username}/${repoName}/contents/README.md`).then(res => res.data);
+  return Axios.get(`${url}/repos/${username}/${repoName}/contents/README.md`, {}, config).then(res => res.data);
 };
 
 export const getUser = username => {
-  return Axios.get(`${url}/users/${username}`).then(res => res.data);
+  return Axios.get(`${url}/users/${username}`, {}, config).then(res => res.data);
 }
