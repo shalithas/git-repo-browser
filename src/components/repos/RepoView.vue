635f69d85734ed469efa272c8250d94170b938e2
Shalitha Senarathne <shalitha.senarathne@insead.edu>
2019-10-03 11:58:57 +0800

    Repo view added

---
+++ src/components/repos/RepoView.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div v-html="content"></div>
+  </div>
+</template>
+<script>
+import { getRepo } from "./repoData.js";
+import marked from "marked";
+
+export default {
+  name: "RepoView",
+  data: () => {
+    return {
+      repo: null,
+      content: ''
+    };
+  },
+  methods: {
+    fetchRepo(username, repoName) {
+      getRepo(username, repoName).then(data => {
+        this.content = marked(atob(data.content));
+        this.repos = data;
+      });
+    }
+  },
+  mounted: function() {
+    this.$nextTick(function() {
+      this.fetchRepo(this.$route.params.username, this.$route.params.repoName);
+    });
+  },
+  watch: {
+    $route(to) {
+      this.fetchRepo(to.params.username, this.$route.params.repoName);
+    }
+  }
+};
+</script>
+<style scoped>
+div {
+  text-align: left;
+}
+</style>
--- src/components/routes.js
+++ src/components/routes.js
@@ -1,7 +1,9 @@
 import SearchPage from './SearchPage.vue';
 import RepoList from './repos/RepoList.vue';
+import RepoView from './repos/RepoView.vue';
 
 export const routes = [
+    { path: '/:username/:repoName', component: RepoView },
     { path: '/:username', component: RepoList },
     { path: '/', component: SearchPage },
 ];
